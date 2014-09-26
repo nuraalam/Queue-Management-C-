93c16b81bc0ee63471b3e07b33d8ff95f60ce1bb
nuraalam <aklima.alam13@gmail.com>
2014-09-26 08:43:57 +0600

    First Commit

---
+++ QueueManagmentUserDefineApp/QueueManagmentUserDefineApp/CustomarDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueManagmentUserDefineApp
+{
+    class CustomarDetails
+    {
+        public static Queue<string> CustomarNameQueue=new Queue<string>();
+        public static Queue<string> CustomarComplainQueue=new Queue<string>();
+        public  static Queue<int> CustomerSerialNumberQueue=new Queue<int>();
+        public static int SerialNumber = 1;
+    }
+}
---
+++ QueueManagmentUserDefineApp/QueueManagmentUserDefineApp/QueueManagmentUserDefineApp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueueManagmentUserDefineApp
+{
+    public partial class QueueManagmentUserDefineApp : Form
+    {
+        public QueueManagmentUserDefineApp()
+        {
+            InitializeComponent();
+        }
+
+        private void enqueueButton_Click(object sender, EventArgs e)
+        {
+            if ((enqueueNameTextBox.Text == "") || (enqueueComplainTextBox.Text == ""))
+            {
+                MessageBox.Show("Entry is missing");
+                return;
+            }
+            CustomarDetails.CustomarNameQueue.Enqueue(enqueueNameTextBox.Text);
+            CustomarDetails.CustomarComplainQueue.Enqueue(enqueueComplainTextBox.Text);
+            CustomarDetails.CustomerSerialNumberQueue.Enqueue(CustomarDetails.SerialNumber);
+            string msg = enqueueNameTextBox.Text + "'s serial number is " + CustomarDetails.SerialNumber;
+            MessageBox.Show(msg);
+            var items = new ListViewItem(CustomarDetails.SerialNumber.ToString());
+            items.SubItems.Add(enqueueNameTextBox.Text);
+            items.SubItems.Add(enqueueComplainTextBox.Text);
+            queueManagementListView.Items.Add(items);
+            enqueueNameTextBox.Text = "";
+            enqueueComplainTextBox.Text = "";
+            CustomarDetails.SerialNumber++;
+
+        }
+
+        private void dequeueButton_Click(object sender, EventArgs e)
+        {
+            if (CustomarDetails.CustomarNameQueue.Count == 0)
+            {
+                dequeueSerialNoTextBox.Text = "";
+                dequeueNameTextBox.Text = "";
+                dequeueComplainTextBox.Text = "";
+                MessageBox.Show("Congratulation! There is no customer in the queue");
+                return;
+            }
+            dequeueSerialNoTextBox.Text = CustomarDetails.CustomerSerialNumberQueue.Dequeue().ToString();
+            dequeueNameTextBox.Text = CustomarDetails.CustomarNameQueue.Dequeue();
+            dequeueComplainTextBox.Text = CustomarDetails.CustomarComplainQueue.Dequeue();
+            queueManagementListView.Items.RemoveAt(0);
+          
+        }
+    }
+}
